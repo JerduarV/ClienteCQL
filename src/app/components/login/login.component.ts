a2277f324bcbaba24f091f9c0e084285f9ea3667
JerduarV <201442819@ingenieria.usac.edu.gt>
2019-09-23 11:19:40 -0600

    Se agregó la opción de escoger el nivel. Solo funciona avanzado

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from 'src/app/services/login.service';
+import { Router } from '@angular/router';
+import { User } from 'src/app/Auxiliares/User';
+import { UserServiceService } from 'src/app/services/user-service.service';
+
+declare var InterpretarLup: any;
+declare var parsear: any;
 
 @Component({
   selector: 'app-login',
@@ -8,17 +14,41 @@ import { LoginService } from 'src/app/services/login.service';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private loginService: LoginService) { }
+  constructor(private loginService: LoginService, private router: Router,
+    private userService: UserServiceService) { }
 
   ngOnInit() {
   }
 
-  private login(user: string, pwd: string, event: Event){
+  private login(user: string, pwd: string, nivel: string,  event: Event){
+    
     event.preventDefault();
+
+    let a;
 
     this.loginService.login(user, pwd).subscribe(
       res =>{
         console.log(res);
+        (function ($) {
+          a = $(res);
+        })(parsear);
+        console.log(a);
+
+        (function ($) {
+          a = $(a, null)
+        })(InterpretarLup);
+        if(!a){
+          alert('USUARIO O CONTRAEÑA INCORRECTA');
+        }else{
+          let u: User = new User(user);
+          this.userService.setUsuarioLogeado(u);
+
+          if(nivel == 'Avanzado'){
+            this.router.navigate(['/main']);
+          }else{
+            alert('SELECCIONE UN NIVEL');
+          }
+        }
       }
     );
   }
